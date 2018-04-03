6a28fc633da2dc9da487715f04643bb44f32375f
gaiusyao <gaiusyao@yeah.net>
2018-04-03 15:14:03 +0800

    update

--- pythagoras/iDash/views.py
+++ pythagoras/iDash/views.py
@@ -5,5 +5,5 @@ from django.http import HttpResponse
 def index(request):
     return render(request, 'iDash/index.html', context={
                       'title': 'iDash首页', 
-                      'logo': 'iDash'
+                      'brand': 'iDash'
                   })
